cdf1c6d4d4dcb05aa4024c6eede655e1cf652566
codingpeasant <zhaofanzf@gmail.com>
2019-11-14 22:35:17 -0500

    updating js logos

--- client/example.js
+++ client/example.js
@@ -1,13 +1,13 @@
-const Blocase = require('./index.js')
+const Blocace = require('./index.js')
 
 // initializing the default admin account
-var blocase = Blocase.createFromPrivateKey('44e654a98faf8e608bf833dd8a5e7bd448c4c7118008208917ca06d48254ff12')
+var blocace = Blocace.createFromPrivateKey('44e654a98faf8e608bf833dd8a5e7bd448c4c7118008208917ca06d48254ff12')
 
 // encrypt and decrypt the seed
-var encryptPrivKey = blocase.encryptPrivateKey('123456')
-var decryptPrivKey = Blocase.decryptPrivateKey(encryptPrivKey, '123456')
+var encryptPrivKey = blocace.encryptPrivateKey('123456')
+var decryptPrivKey = Blocace.decryptPrivateKey(encryptPrivKey, '123456')
 
-console.log(blocase.wallet.getPrivateKey().toString('hex') === decryptPrivKey)
+console.log(blocace.wallet.getPrivateKey().toString('hex') === decryptPrivKey)
 
 // user account payload
 const accountPayload = {
@@ -86,74 +86,74 @@ function timeout (ms) {
 async function start () {
   try {
     // get JWT
-    const jwt = await blocase.getJWT()
+    const jwt = await blocace.getJWT()
     console.log(jwt)
 
     // register account
-    const accountRes = await blocase.createAccount(accountPayload)
+    const accountRes = await blocace.createAccount(accountPayload)
     console.log(accountRes.data.address)
 
     // get account
-    const account = await blocase.getAccount(accountRes.data.address)
+    const account = await blocace.getAccount(accountRes.data.address)
     console.log(account)
 
     // update account
     accountPayload.email = 'asd@asd.com'
     accountPayload.publicKey = '3ecc361be41aa06e0b2a9a7c65c1c750ad3ab503367180502e61c5f4b6f8e8b4e8d1d218042631546a5fe694e719b7a23f343a81fc25abcbd6f2f8c25d398d08'
-    const accountUpdateRes = await blocase.updateAccount(accountPayload, accountRes.data.address)
+    const accountUpdateRes = await blocace.updateAccount(accountPayload, accountRes.data.address)
     console.log(accountUpdateRes.data.message)
 
     // set account read / write permission
-    const accountPermissionRes = await blocase.setAccountReadWrite(permission, accountRes.data.address)
+    const accountPermissionRes = await blocace.setAccountReadWrite(permission, accountRes.data.address)
     console.log(accountPermissionRes.data.message)
 
     // get account again
-    const accountUpdated = await blocase.getAccount(accountRes.data.address)
+    const accountUpdated = await blocace.getAccount(accountRes.data.address)
     console.log(accountUpdated)
     
     // create collection
-    const collectionCreationRes = await blocase.createCollection(collectionMappingPaylod)
+    const collectionCreationRes = await blocace.createCollection(collectionMappingPaylod)
     console.log(collectionCreationRes)
 
     // create the user account
-    var blocaseUser = Blocase.createFromPrivateKey('277d271593d205c6078964c31fb393303efd76d5297906f60d2a7a7d7d12c99a')
+    var blocaceUser = Blocace.createFromPrivateKey('277d271593d205c6078964c31fb393303efd76d5297906f60d2a7a7d7d12c99a')
     // get user JWT
-    const jwtUser = await blocaseUser.getJWT()
+    const jwtUser = await blocaceUser.getJWT()
     console.log(jwtUser)
 
     // add document
-    const putDocRes = await blocaseUser.signAndPutDocument(document, 'new1')
+    const putDocRes = await blocaceUser.signAndPutDocument(document, 'new1')
     console.log(putDocRes)
 
     // wait for block to be generated
     await timeout(2000)
 
     // query the database
-    const queryRes = await blocaseUser.query(queryPayload, 'new1')
+    const queryRes = await blocaceUser.query(queryPayload, 'new1')
     console.log(queryRes)
 
     // verify if the transaction is included in the block
-    const verificationPassed = await blocaseUser.verifyTransaction(queryRes.hits[0]._blockId, queryRes.hits[0]._id)
+    const verificationPassed = await blocaceUser.verifyTransaction(queryRes.hits[0]._blockId, queryRes.hits[0]._id)
     console.log(verificationPassed)
 
     // get block information
-    const blockRes = await blocase.getBlockInfo(queryRes.hits[0]._blockId)
+    const blockRes = await blocace.getBlockInfo(queryRes.hits[0]._blockId)
     console.log(blockRes)
 
     // get blockchain information
-    const blockchainRes = await blocase.getBlockchainInfo(queryRes.hits[0]._blockId)
+    const blockchainRes = await blocace.getBlockchainInfo(queryRes.hits[0]._blockId)
     console.log(blockchainRes)
 
     // get all collections
-    const collectionsRes = await blocase.getCollections()
+    const collectionsRes = await blocace.getCollections()
     console.log(collectionsRes)
 
     // get collection data schema
-    const collectionRes = await blocase.getCollection('new1')
+    const collectionRes = await blocace.getCollection('new1')
     console.log(JSON.stringify(collectionRes))
 
     // verify signature
-    console.log(Blocase.verifySignature(queryRes.hits[0]._source, queryRes.hits[0]._signature, blocaseUser.wallet.getPublicKey().toString('hex')))
+    console.log(Blocace.verifySignature(queryRes.hits[0]._source, queryRes.hits[0]._signature, blocaceUser.wallet.getPublicKey().toString('hex')))
   } catch (error) {
     console.log(error.response)
   }
